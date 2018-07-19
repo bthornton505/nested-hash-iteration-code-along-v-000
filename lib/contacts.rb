2b03919081d1b7674ad79e0b6d60b093ed93d18c
Brenden Thornton <thorntonbrenden@gmail.com>
2018-07-19 00:17:03 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,7 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    person.each do |attribute, info|
+    contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
   end 
         
